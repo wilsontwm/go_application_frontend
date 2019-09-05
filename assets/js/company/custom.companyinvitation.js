3140e0f49970b68ac964909dea7b7ba31a598721
Wilson Tan <wilson.tanwm@gmail.com>
2019-09-06 00:24:20 +0800

    Added sender info on the invitation request

--- assets/js/company/custom.companyinvitation.js
+++ assets/js/company/custom.companyinvitation.js
@@ -1,11 +1,36 @@
 $(document).ready(function(){    
     $invitationForm = document.getElementById("respond-company-invitation-form");
+    $(".invitation-link").click(function(e) {
+        e.preventDefault();
+        var $row = $(this).closest(".invitation-row");
+        if($row.length > 0) {
+            var $btn = $row.find(".btn-view-invitation");
+            if($btn.length > 0) {
+                $btn.click();
+            }
+        }
+    });
+
     $(".btn-view-invitation").click(function(){
         var id = $(this).data('id');
         var companyName = $(this).data('company');
         var status = $(this).data('status');
+        var message = $(this).data('message');
+        var senderName = $(this).data('sender');
+        var senderEmail = $(this).data('senderemail');
+
         $invitationForm.action = "/dashboard/invite/incoming/" + id + "/respond";
         $("#company-name-container").html(companyName);
+        if(message.length > 0) {
+            var messageString = '<figure class="quote"><div class="quote-body">' + message + '</div></figure>';
+            $("#message-container").html(messageString);
+        } else {
+            $("#message-container").html('');
+        }
+        $("#sender-name-container").html(senderName);
+        var senderEmailString = '<a href="mailto:' + senderEmail + '">' + senderEmail + '</a>';
+        $("#sender-email-container").html(senderEmailString);
+
         if(parseInt(status) == 0) {
             $("#btn-accept-invitation").attr("disabled", false);
             $("#btn-decline-invitation").attr("disabled", false);
